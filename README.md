1ed070d44dc6fa7b6d9c3c941915b31ac94b0e1f
Manuel Fernandez <manferlo81@gmail.com>
2019-11-05 14:11:52 +0700

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bundlib-starter
-My awesome library built using Bundlib
+
+My awesome library built using [Bundlib](https://github.com/manferlo81/bundlib)
+
+## README
+
+Edit this file to fit your needs.
+
+## LICENSE
+
+We DO NOT include a license in this project as it is intended to be used as a template. Generate a LICENSE after you create your project.
---
+++ jest.config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+
+  testEnvironment: 'node',
+  cacheDirectory: 'node_modules/.cache/jest',
+
+  preset: 'ts-jest',
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'json',
+    'lcov',
+    'clover',
+    'text',
+    'text-summary',
+  ],
+
+  verbose: true,
+
+}
